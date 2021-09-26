61b3ca9d4abd21f8cf76e489a3fc97fa14968fef
Iuri <iurirodigues2200@gmail.com>
2021-09-26 09:23:51 -0300

    install docker

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# install dependencies 
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
+# add the gpg key for docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# add the repository in the Linux mint 20
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
+
+sudo apt-get update
+
+# install docker e docker-compose
+sudo apt install docker-ce docker-compose
+
+# add the user system to sudo group, no sudo command
+sudo usermod -aG docker $USER
+
+# prints docker version =)
+docker --version
